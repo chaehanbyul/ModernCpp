cc7771670fffe3f887e345e40f9dcf07d0f0cda1
Chae Hanbyul <chb423@naver.com>
2021-05-23 15:07:17 +0900

    Input Output
    입출력 예제 진행 중

--- 1st/ExampleCode/InputOutput.cpp
+++ 1st/ExampleCode/InputOutput.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <iomanip>
+#include <math.h>
+#include <corecrt_math_defines.h>
+
+#define _USE_MATH_DEFINES
+using namespace std;
 
 //Wirte File
 void write_squares_fille()
@@ -25,6 +31,28 @@ void write_something(std::ostream& os)
 	os << "Hi stream, did you know that 3 * 3 = " << 3 * 3 << std::endl;
 }
 
+void set_format()
+{
+	double pi = M_PI;
+	cout << "pi is " << pi << '\n';	//pi is 3.14159
+	cout << "pi is " << setprecision(16) << pi << '\n';	//pi is 3.141592653589793
+
+	cout << "pi is " << setw(30) << pi << '\n';	//pi is              3.141592653589793
+
+	cout << "pi is " << setfill('-') << left << setw(30) << pi << '\n';	//pi is 3.141592653589793-------------
+
+	cout.setf(ios_base::showpos);	//앞으로 양수에도 부호를 표시
+	cout << "pi is " << scientific << pi << '\n';	//pi is+3.1415926535897931e+00
+
+
+	cout << "63 octal is " << oct << 63 << '\n';	//63 octal is 77
+	cout << "63 hex is " << hex << 63 << '\n';	//63 hex is 3f
+	cout << "63 decimal is " << dec << 63 << '\n';	//63 decimal is + 63
+
+	cout << "pi < 3 is " << (pi < 3) << '\n';
+	cout << "pi < 3 is " << boolalpha << (pi < 3) << '\n';
+
+}
 
 int main()
 {
@@ -37,6 +65,10 @@ int main()
 	write_something(mystream);
 
 	std::cout << "mystream is: " << mystream.str();	//개행을 포함하고 있다.
+
+	myfile.close();
+
+	set_format();
 
 	return 0;
 }
